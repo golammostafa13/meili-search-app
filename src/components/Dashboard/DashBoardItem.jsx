5464c88c8bf23dda1e2a2a2dacd020ac5e3dcb06
mostafarmstu <mostafarmstu@gmail.com>
2023-06-13 11:04:35 +0600

    Update: style changed

--- src/components/Dashboard/DashBoardItem.jsx
+++ src/components/Dashboard/DashBoardItem.jsx
@@ -5,10 +5,10 @@ const DashBoardItem = ({ item, searchTerm }) => {
   const { poster, url } = item;
 
   return (
-    <div className="h-fit w-[50%] bg-gray-800 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-0 border border-indigo-500 my-2 shadow-lg flex dark:border-gray-700 dark:shadow-md dark:shadow-slate-800">
-      <a href={url} className="w-1/3 flex items-center justify-center p-6 border-r-2 border-indigo-50 dark:border-gray-700" download="download.pdf" target="_blank" rel="noreferrer">
+    <div className="h-[330px] w-[40%] bg-gray-800 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-0 border border-indigo-500 my-2 shadow-lg flex items-center dark:border-gray-700 dark:shadow-md dark:shadow-slate-800">
+      <a href={url} className="h-[310px] w-1/3 flex items-center justify-center p-2 border-r-2 border-indigo-50 dark:border-gray-700" download="download.pdf" target="_blank" rel="noreferrer">
         <img
-          className="border-2 border-gray-200 shadow-xl cursor-pointer hover:scale-105 transition-all duration-500 dark:border-white"
+          className="border-2 h-full w-fit border-gray-200 shadow-xl cursor-pointer hover:scale-105 transition-all duration-500 dark:border-white"
           src={poster}
           alt="poster"
         />
