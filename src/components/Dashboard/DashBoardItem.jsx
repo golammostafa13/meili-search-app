73e216f9c906515f586926393ed104c021f39b6c
mostafarmstu <mostafarmstu@gmail.com>
2023-06-19 12:28:47 +0600

    update: match case of content

--- src/components/Dashboard/DashBoardItem.jsx
+++ src/components/Dashboard/DashBoardItem.jsx
@@ -2,7 +2,7 @@ import Description from "./Description";
 
 const DashBoardItem = ({ item, searchTerm }) => {
   const { poster, url } = item;
-
+    
   return (
     <div className="lg:h-[350px] xl:w-[50%] w-[40%] bg-gray-800 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-0 border border-indigo-500 my-2 shadow-lg flex dark:border-gray-700 dark:shadow-md dark:shadow-slate-800">
       <a href={url} className="lg:flex md:hidden h-full w-1/3 items-center justify-center p-2 border-r-2  dark:border-gray-700" download="download.pdf" target="_blank" rel="noreferrer">
